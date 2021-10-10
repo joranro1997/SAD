c6207ef3853708a652ab505365850154b3607df6
sylwiaZon <34057302+sylwiaZon@users.noreply.github.com>
2021-10-10 20:45:45 +0200

    Add promises

--- manipulateStock.js
+++ manipulateStock.js
@@ -7,50 +7,55 @@ module.exports = class StockDb {
     }
 
     interactWithDb(func) {
-        this.mongoclient.connect(this.url,function (err,client) {
-            if (err) throw err;
-
+        this.mongoclient.connect(this.url).then((client) => {
             const db = client.db("shop");
 
             db.listCollections().toArray().then((docs) => {
                 if(!docs.map(doc => doc.name).includes('products')) {
-                    db.createCollection('products', function(err, res) {
-                        if (err) throw err;
+                    db.createCollection('products').then(() => {
                         console.log("Collection created!");
+                    }).catch((err) => {
+                        console.log(err);
                     });
+
                     db.members.createIndex( { "cod": 1 }, { unique: true } )
                 }
             }).catch((err) => {
                 console.log(err);
             });
 
-            func(db,function() {
-                if (err) throw err;
-                client.close();
-            });
+            func(db,client);
+            
+        }).catch((err) => {
+            throw err;
         });
     }
 
-    insertGoods(db, callback) {
+    insertGoods(db, client) {
         var collection = db.collection('products');
-        collection.insertMany([
-            {_id: 1, cod : 1,desc:'palos',stock:0}, {_id: 2, cod : 2,desc:'hierros',stock:10}, {_id: 3,cod : 3,desc:'muelles',stock:5}
-        ], function(err, result) {
-            callback(result);
+        let query = { _id: 1 };
+        collection.findOne(query).then((res) => {
+            if(res == null) {
+                collection.insertMany([
+                    {_id: 1, cod : 1,desc:'palos',stock:0}, {_id: 2, cod : 2,desc:'hierros',stock:10}, {_id: 3,cod : 3,desc:'muelles',stock:5}
+                ]);
+            }
+            client.close();
         });
     }
 
-    checkGoods(good, db, callback) {
+    checkGoods(good, db, client) {
         var collection = db.collection('products');
         let query = { desc: good };
-        collection.findOne(query, function(err, result) {
-            if (err) throw err;
+        collection.findOne(query).then((result) => {
             if(result.stock) {
                 console.log({product: good, success: "Exists"});
             } else {
                 console.log({product: good, error: "Not in stock"});
             }
-            callback(result);
+            client.close();
+        }).catch((err) => {
+            throw err;
         });
     }
 
