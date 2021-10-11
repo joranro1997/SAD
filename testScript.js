3b321ea79c1d932d2256f9e5e8495d859d7597e4
joranro1997 <66689725+joranro1997@users.noreply.github.com>
2021-10-11 18:29:32 +0200

    Update testScript.js

--- testScript.js
+++ testScript.js
@@ -1,7 +1,8 @@
+//The exported module is imported into the test script.
 var cartMaker = require('./CartMaker.js');
-
+//We create an instance of the class from the exported module.
 let cart = new cartMaker('cart1', ['pizza', 'beer'])
-
+//We call different functions from the newly created instance of the exported class.
 cart.addItem('melon');
 
 cart.toString();
